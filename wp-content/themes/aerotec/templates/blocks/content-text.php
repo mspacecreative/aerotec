9d5f4d4a1b3bf7ae30291306381acc1d8564b3e9
Matt <matt@mspacecreative.com>
2019-02-21 09:23:57 -0400

    c

--- wp-content/themes/aerotec/templates/blocks/content-text.php
+++ wp-content/themes/aerotec/templates/blocks/content-text.php
@@ -1,3 +1,7 @@
-<?php if ( get_field('text_editor') ): ?>
-	<?php the_field('text_editor'); ?>
-<?php endif; ?>
+<div class="section_container">
+	<div class="row_container">
+		<?php if ( get_field('text_editor') ): ?>
+			<?php the_field('text_editor'); ?>
+		<?php endif; ?>
+	</div>
+</div>
