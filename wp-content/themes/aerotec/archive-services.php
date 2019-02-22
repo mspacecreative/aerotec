49321d24d6242687337f6d5ef96450ac012af096
Matt <matt@mspacecreative.com>
2019-02-22 16:15:50 -0400

    c

--- wp-content/themes/aerotec/archive-services.php
+++ wp-content/themes/aerotec/archive-services.php
@@ -3,6 +3,9 @@
 <div id="main-content">
 	<div class="container">
 		<div id="content-area" class="clearfix">
+			<div class="section-title-container boxed">
+				<h1 class="section-title"><?php echo post_type_archive_title( '', false ); ?></h1>
+			</div>
 			<?php get_template_part('includes/loop-services'); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
