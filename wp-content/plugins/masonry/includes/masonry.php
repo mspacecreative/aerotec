a7b4b1565c59aa4512825a0352bc1facf05f9f68
Matt <matt@mspacecreative.com>
2019-02-23 16:56:54 -0400

    c

--- wp-content/plugins/masonry/includes/masonry.php
+++ wp-content/plugins/masonry/includes/masonry.php
@@ -3,34 +3,34 @@
 	<div class="gutter-sizer"></div>
 	
 	<?php 
-	if ( have_posts() ) : 
-		while ( have_posts() ) : the_post(); ?>
-		<div class="grid-item">
-			<div class="grid-inner">
-				<?php if ( has_post_thumbnail() ) { ?>
-				    <?php echo the_post_thumbnail(); ?>
-				<?php } ?>
-					<div class="card-content">
-						<h3><?php the_title(); ?></h3>
-						<?php
-						if( has_excerpt() ) { 
-							the_excerpt(); 
-						} else {
-							the_content();
-						}
-						?>
-						<?php if ( get_field( 'custom_link' ) ): ?>
-						<a class="read-more-link" href="<?php the_field('custom_link'); ?>"><?php _e(' More...'); ?></a>
-						<?php elseif ( get_field( 'external_link' ) ): ?>
-						<a class="read-more-link" href="<?php the_field('external_link'); ?>"><?php _e(' More...'); ?></a>
-						<?php else: ?>
-						<a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e(' More...'); ?></a>
-					<?php endif; ?>
-					</div>
+	$loop = new WP_Query( array( 'post_type' => 'services', 'posts_per_page' => -1 ) );
+	    if ( $loop->have_posts() ) :
+	        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			<div class="grid-item">
+				<div class="grid-inner">
+					<?php if ( has_post_thumbnail() ) { ?>
+					    <?php echo the_post_thumbnail(); ?>
+					<?php } ?>
+						<div class="card-content">
+							<h3><?php the_title(); ?></h3>
+							<?php
+							if( has_excerpt() ) { 
+								the_excerpt(); 
+							} else {
+								the_content();
+							}
+							?>
+							<?php if ( get_field( 'custom_link' ) ): ?>
+							<a class="read-more-link" href="<?php the_field('custom_link'); ?>"><?php _e(' More...'); ?></a>
+							<?php elseif ( get_field( 'external_link' ) ): ?>
+							<a class="read-more-link" href="<?php the_field('external_link'); ?>"><?php _e(' More...'); ?></a>
+							<?php else: ?>
+							<a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e(' More...'); ?></a>
+						<?php endif; ?>
+						</div>
+				</div>
 			</div>
-		</div>
-		<?php endwhile; 
-	endif;
-	?>
+			<?php endwhile; 
+		endif; wp_reset_postdata(); ?>
 	
 </div>
