590388fda5cf2ab4132c04c56c3a93769eefc567
Matt <matt@mspacecreative.com>
2019-02-26 16:28:07 -0400

    c

--- wp-content/themes/aerotec/templates/blocks/content-testimonials-loop.php
+++ wp-content/themes/aerotec/templates/blocks/content-testimonials-loop.php
@@ -2,5 +2,5 @@
 	<div class="title-container">
 		<h1 class="section-title"><?php esc_html_e('Testimonials'); ?></h1>
 	</div>
-	<?php get_template_part('includes/loop-testimonials.php'); ?>
+	<?php get_template_part('includes/loop-testimonials'); ?>
 </div>
