6b0ae3f22638e0cd49a246727b2bfbc7c962b79c
Matt <matt@mspacecreative.com>
2019-02-26 15:56:42 -0400

    c

--- wp-content/themes/aerotec/archive-services.php
+++ wp-content/themes/aerotec/archive-services.php
@@ -15,7 +15,7 @@ if ( $narrow ): ?>
 			<div class="section-title-container boxed">
 				<h1 class="section-title"><?php echo post_type_archive_title( '', false ); ?></h1>
 			</div>
-			<?php get_template_part('includes/loop-services'); ?>
+			<?php get_template_part('includes/loop-testimonials'); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
 </div> <!-- #main-content -->
