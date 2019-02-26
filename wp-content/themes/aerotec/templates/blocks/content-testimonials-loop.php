e0b81bed30b8e89019fe9d75d9e2fcedaeb40c66
Matt <matt@mspacecreative.com>
2019-02-26 16:23:47 -0400

    c

--- wp-content/themes/aerotec/templates/blocks/content-testimonials-loop.php
+++ wp-content/themes/aerotec/templates/blocks/content-testimonials-loop.php
@@ -1,21 +1,6 @@
-<div id="instafeed">
-	<div class="grid-sizer"></div>
-	<div class="gutter-sizer"></div>
-	
-	<?php 
-	$loop = new WP_Query( array( 'post_type' => 'testimonials', 'posts_per_page' => -1 ) );
-	    if ( $loop->have_posts() ) :
-	        while ( $loop->have_posts() ) : $loop->the_post(); ?>
-			<div class="grid-item">
-				<div class="grid-inner">
-					<div class="card-content">
-						<h3><?php the_title(); ?></h3>
-						<?php the_content(); ?>
-					</div>
-				</div>
-			</div>
-			<?php endwhile; 
-		endif; wp_reset_postdata();
-	?>
-	
+<div class="testimonials-loop-container">
+	<div class="title-container">
+		<h1 class="section-title"><?php esc_html_e('Testimonials'); ?></h1>
+	</div>
+	<?php get_template_part('includes/loop-testimonials.php'); ?>
 </div>
