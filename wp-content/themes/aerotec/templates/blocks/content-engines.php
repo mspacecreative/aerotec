e3a35574165729547274aec04b61536750205708
Matt <matt@mspacecreative.com>
2019-03-11 19:09:25 -0300

    c

--- wp-content/themes/aerotec/templates/blocks/content-engines.php
+++ wp-content/themes/aerotec/templates/blocks/content-engines.php
@@ -1,9 +1,11 @@
 <div class="section_container">
 	<div class="row_container clearfix">
+		<?php if( have_rows('lycoming') ):
+		while( have_rows('lycoming') ): the_row(); ?>
 		<div class="one_half_col">
 			<?php
-			$lycominglogo = get_field('lycoming_logo');
-			$lycomingengine = get_field('lycoming_engine');
+			$lycominglogo = get_sub_field('lycoming_logo');
+			$lycomingengine = get_sub_field('lycoming_engine');
 			$size = 'medium'; 
 			
 			if( $lycominglogo == 'true' && $lycomingengine == 'true' ) { ?>
@@ -13,10 +15,16 @@
 			</div>
 			<?php } ?>
 		</div>
+		<?php 
+		endwhile;
+		endif; ?>
+		
+		<?php if( have_rows('continental') ):
+		while( have_rows('continental') ): the_row(); ?>
 		<div class="one_half_col">
 			<?php
-			$continentallogo = get_field('continental_logo');
-			$continentalengine = get_field('continental_engine');
+			$continentallogo = get_sub_field('continental_logo');
+			$continentalengine = get_sub_field('continental_engine');
 			$size = 'medium'; 
 			
 			if( $continentallogo == 'true' && $continentalengine == 'true' ) { ?>
@@ -26,5 +34,8 @@
 			</div>
 			<?php } ?>
 		</div>
+		<?php 
+		endwhile;
+		endif; ?>
 	</div>
 </div>
