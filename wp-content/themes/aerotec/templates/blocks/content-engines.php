b16580db13fb0918fd8fac2f3298181200e0c2a8
Matt <matt@mspacecreative.com>
2019-03-12 11:15:17 -0300

    c

--- wp-content/themes/aerotec/templates/blocks/content-engines.php
+++ wp-content/themes/aerotec/templates/blocks/content-engines.php
@@ -11,7 +11,7 @@
 			$lycomingengine = get_sub_field('lycoming_engine');
 			$size = 'medium'; ?>
 	
-			<a href="<?php the_field('external_link', $lycominglogo['ID']); ?>" target="_blank">
+			<a href="<?php the_field('external_link'); ?>" target="_blank">
 				<?php echo wp_get_attachment_image( $lycominglogo, $size ); ?>
 			</a>
 			<?php endif; ?>
