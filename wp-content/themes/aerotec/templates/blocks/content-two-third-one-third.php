87592c1db434b418457e59f9e203ab194b0ae763
Matt <matt@mspacecreative.com>
2020-04-13 12:47:46 -0300

    c

--- wp-content/themes/aerotec/templates/blocks/content-two-third-one-third.php
+++ wp-content/themes/aerotec/templates/blocks/content-two-third-one-third.php
@@ -1303,6 +1303,643 @@ elseif ( $flex == 'center' ):
 	</div>
 	<?php endif;
 	
+elseif ( $flex == 'top' ) :
+	
+	if ( $blockanchor && $padding == 'top' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-padding top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'top' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-padding top-margin display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'bottom' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		<div class="row gutter_space_3 between-lg between-md width-800 clear bottom-padding bottom-margin display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'top' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-padding bottom-margin display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'bottom' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear bottom-padding top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'both' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-bottom-padding top-margin display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'both' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-bottom-padding bottom-margin display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'both' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-bottom-padding top-bottom-margin">
+		
+		<?php include('includes/two-third-one-third-loop.php'); ?>
+		
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'top' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'bottom' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear bottom-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'top'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'bottom'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $padding == 'both'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $margin == 'top'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $margin == 'bottom'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $width && $margin == 'both'): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md width-800 clear top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'top' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-padding top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear bottom-padding bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'bottom' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear bottom-padding bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-bottom-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'both' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-bottom-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'top' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-padding bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'top' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-padding bottom-margin">
+		
+		<?php include('includes/two-third-one-third-loop.php'); ?>
+		
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'top' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-padding top-bottom-margin">
+		
+		<?php include('includes/two-third-one-third-loop.php'); ?>
+		
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'top' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-padding top-bottom-margin">
+		
+		<?php include('includes/two-third-one-third-loop.php'); ?>
+		
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear bottom-padding top-margin">
+		
+		<?php include('includes/two-third-one-third-loop.php'); ?>
+		
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'bottom' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear bottom-padding top-margin">
+				
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+				
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear bottom-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'bottom' && $margin == 'both' ): ?>
+	<div class="row_container">
+		<div class="row gutter_space_3 between-lg between-md clear bottom-padding top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-bottom-padding top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'both' && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-bottom-padding top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-bottom-padding bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'both' && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-bottom-padding bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $padding == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $padding == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-bottom-padding">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $margin == 'bottom' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $margin == 'top' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $blockanchor && $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div id="<?php the_field('block_id'); ?>" class="row gutter_space_3 between-lg between-md clear top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php elseif ( $margin == 'both' ): ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear top-bottom-margin">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	
+	<?php else : ?>
+	<div class="row_container">
+		
+		<?php if ( get_field('heading') ): ?>
+		<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+		<?php endif; ?>
+		
+		<div class="row gutter_space_3 between-lg between-md clear display-flex">
+			
+			<?php include('includes/two-third-one-third-loop.php'); ?>
+			
+		</div>
+	</div>
+	<?php endif;
+	
 else :
 	
 	if ( $blockanchor && $padding == 'top' && $margin == 'top' ): ?>
