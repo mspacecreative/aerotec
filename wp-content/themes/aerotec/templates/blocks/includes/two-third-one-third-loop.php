b114d8155665c50126b356df1b3adad2aa93e7d1
Matt <matt@mspacecreative.com>
2020-04-13 12:30:33 -0300

    c

--- wp-content/themes/aerotec/templates/blocks/includes/two-third-one-third-loop.php
+++ wp-content/themes/aerotec/templates/blocks/includes/two-third-one-third-loop.php
@@ -45,14 +45,15 @@ else : ?>
 </div>
 
 <?php if( have_rows('one_third_column') ): ?>
-<?php while( have_rows('one_third_column') ): the_row(); ?>
+<?php while( have_rows('one_third_column') ): the_row();
+$contenttype = get_sub_field('content_type'); ?>
 
-	<?php if ( get_sub_field('content_type') == 'text' ): ?>
+	<?php if ( $contenttype == 'text' ): ?>
 	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-container">
 		<?php the_sub_field('text_editor'); ?>
 	</div>
 	
-	<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+	<?php elseif ( $contenttype == 'image' ): ?>
 	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-container">
 		<?php 
 		$image = get_sub_field('image_upload');
@@ -64,9 +65,34 @@ else : ?>
 		?>
 	</div>
 	
-	<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+	<?php elseif ( $contenttype == 'video' ): ?>
 	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-container">
 		<?php the_sub_field('video_upload'); ?>
+	</div>
+	
+	<?php elseif ( $contenttype == 'carousel' ): ?>
+	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-container">
+		<div class="carousel who_we_are">
+					
+			<?php
+			$images = get_sub_field('photo_gallery');
+			$size = 'large';
+					
+			if( $images ):
+			foreach( $images as $image ): ?>
+					
+			<div>
+				<img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+				<?php if ( $image ): ?>
+				<p style="margin: 15px 0 0; text-align: center;"><?php echo $image['title']; ?></p>
+				<?php endif; ?>
+			</div>
+					
+			<?php 
+			endforeach;
+			endif; ?>
+					
+		</div>
 	</div>
 	<?php endif; ?>
 
