8437707d8057f084352c0b91409c33c67d964616
Matt <matt@mspacecreative.com>
2020-04-16 18:07:35 -0300

    c

--- wp-content/themes/aerotec/templates/blocks/content-warranty.php
+++ wp-content/themes/aerotec/templates/blocks/content-warranty.php
@@ -1,6 +1,6 @@
 <div class="warranty-section clearfix">
 	<div class="warranty-badge">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/warranty-badge.svg" />
+		<a href="/resources/warranty/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/warranty-badge.svg" /></a>
 	</div>
 	<?php if ( get_field('warranty_blurb') ): ?>
 	<div class="one-half">
@@ -10,7 +10,7 @@
 	</div>
 	<?php endif; ?>
 	<?php if ( get_field('warranty_img') ): ?>
-	<a href="/resources/warranty/"><div class="one-half bg-img" style="background-image: url(<?php the_field('warranty_img'); ?>);"></div>
+	<div class="one-half bg-img" style="background-image: url(<?php the_field('warranty_img'); ?>);">
 		
 	</div>
 	<?php endif; ?>
